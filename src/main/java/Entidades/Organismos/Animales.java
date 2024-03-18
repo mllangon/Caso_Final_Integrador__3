9d1aa2ac431ed7c1f60d28f2ddf537416fef43ba
mllangon <mllangon@myuax.com>
2024-03-18 18:13:15 +0100

    2

--- src/main/java/Entidades/Organismos/Animales.java
+++ src/main/java/Entidades/Organismos/Animales.java
@@ -1,4 +1,28 @@
 package Entidades.Organismos;
 
-public class Animales {
+public class Animales extends Organismo {
+    private String especie;
+    private String alimentacion;
+
+    public Animales(Posicion posicion, int salud, int edad, boolean estadoReproductivo, String especie, double peso) {
+        super(posicion, salud, edad, estadoReproductivo);
+        this.especie = especie;
+        this.alimentacion = alimentacion;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String Alimentacion) {
+        this.alimentacion = alimentacion;
+    }
 }
