f647ea685ff5406f349276af2ae3c6a3d3ba8622
mllangon <mllangon@myuax.com>
2024-03-18 18:52:47 +0100

    7

--- src/main/java/D_Poblacionales/Crec_y_Rep/Crecimiento.java
+++ src/main/java/D_Poblacionales/Crec_y_Rep/Crecimiento.java
@@ -1,4 +1,17 @@
 package D_Poblacionales.Crec_y_Rep;
 
+import Entidades.Organismos.Organismo;
+import java.util.List;
+
 public class Crecimiento {
+
+    public void crecimientoPoblacional(Organismo organismo, double factorCrecimiento) {
+        organismo.setSalud(organismo.getSalud() + (int)(organismo.getSalud() * factorCrecimiento));
+    }
+
+    public void calcularCrecimientoTotal(List<Organismo> organismos, double factorCrecimiento) {
+        for (Organismo organismo : organismos) {
+            crecimientoPoblacional(organismo, factorCrecimiento);
+        }
+    }
 }
