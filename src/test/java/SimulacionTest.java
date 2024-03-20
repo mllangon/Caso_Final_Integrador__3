55371f6041dbe56a9b5c350399956684a57d739f
mllangon <mllangon@myuax.com>
2024-03-20 22:32:56 +0100

    29

---
+++ src/test/java/SimulacionTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SimulacionTest {
+    @Test
+    public void testSimulacion() {
+        Simulacion simulacion = new Simulacion();
+        assertNotNull(simulacion);
+    }
+}
